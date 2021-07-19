13ddc4050cd89c6e565a632e45270286e378c29b
ryowuandjanet <ryowu0329@gmail.com>
2021-07-19 23:08:48 +0800

    20210719-heroku-part6

--- src/settings.py
+++ src/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import src.db as db
 from pathlib import Path
+import subprocess
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
